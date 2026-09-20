f5df48590f74db932f37e5b2e2f6341447db532e
Ombir Kumar <191267158+ombir350@users.noreply.github.com>
2026-09-21 02:36:36 +0530

    Add the Day3 of react program React+vite (npm , Plugins, node, Bundlers)

---
+++ DAY3.2/vite-project/src/App.jsx
@@ -0,0 +1,18 @@
+//export default it can creat only single time.
+export default function App() {
+  return (
+    <h1>Hello Coders</h1>
+  )
+}
+//name-we can name multiplr times
+export function Ombir() {
+  return (
+    <h1>i am coder</h1>
+  )
+}
+
+export function aarti() {
+  return (
+    <h1>she is my sister</h1>
+  )
+}
