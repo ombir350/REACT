da6fc9407be441080a8d99dfbd934cf5524f680c
Ombir Kumar <191267158+ombir350@users.noreply.github.com>
2026-09-24 17:18:51 +0530

    Add the Day5 of react Program (useEffect Hook).

---
+++ DAY5/vite-project/src/main.jsx
@@ -0,0 +1,8 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import Clock from './Clock.jsx'
+
+createRoot(document.getElementById('root')).render(
+    <Clock/>
+)
